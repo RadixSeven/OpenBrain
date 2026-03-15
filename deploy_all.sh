a0ce6973269ed87619832c00d55352441b51d754
Eric Moyer <compu.moyer@gmail.com>
2026-03-15 18:42:21 -0400

    Update docs for env var

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Run this to redeploy everything to Supabase.
 # You must have run `supabase login`, `supabase link`, and
-# set SUPABASE_DB_PASSWORD (in Lastpass) before running this.
+# set SUPABASE_DB_PASSWORD environtment variable
+# (stored in Lastpass and ~/.env) before running this.
 
 supabase functions deploy capture-api --no-verify-jwt
 supabase functions deploy open-brain-mcp --no-verify-jwt
